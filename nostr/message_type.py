c22917d076b88e841564b4916b0afc592eea9f54
jeffthibault <jdthibault2@gmail.com>
2022-08-01 19:45:56 -0400

    add is_valid method to RelayMessageType class

--- nostr/message_type.py
+++ nostr/message_type.py
@@ -7,4 +7,9 @@ class RelayMessageType:
     EVENT = "EVENT"
     NOTICE = "NOTICE"
     END_OF_STORED_EVENTS = "EOSE"
-    
+
+    @staticmethod
+    def is_valid(type: str) -> bool:
+        if type == RelayMessageType.EVENT or type == RelayMessageType.NOTICE or type == RelayMessageType.END_OF_STORED_EVENTS:
+            return True
+        return False
