fb469bdde64a953ebb5a265fa476f14ee9290c06
jeffthibault <jdthibault2@gmail.com>
2022-12-29 14:58:19 -0500

    package setup

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setup(
-    name='nostr',
-    version="0.0.1",
-    packages=find_packages(include=['nostr']),
-    python_requires='>3.6.0',
-    url='https://github.com/jeffthibault/python-nostr',
-    description="A Python library for making Nostr clients.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: MacOS :: MacOS X',
-    ],
-)
+setup()
