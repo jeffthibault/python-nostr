f61f4bc077cbfb863eecbde2460735d6c7be7f73
Ryan Armstrong <ryscar13@gmail.com>
2023-01-21 08:40:11 -0800

    narrow testing assertion

--- test/test_event.py
+++ test/test_event.py
@@ -11,4 +11,4 @@ def test_event_default_time():
     event1 = Event(public_key=public_key, content='test event')
     time.sleep(1.5)
     event2 = Event(public_key=public_key, content='test event')
-    assert event1.created_at != event2.created_at
+    assert event1.created_at < event2.created_at
