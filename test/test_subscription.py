5a13fd0eda7cc97eed9ca2c65a3de936131ca1a3
Ryan Armstrong <ryscar13@gmail.com>
2023-01-21 10:45:03 -0800

    test for subscription id

---
+++ test/test_subscription.py
@@ -0,0 +1,15 @@
+import json
+from nostr.filter import Filter, Filters
+from nostr.message_type import ClientMessageType
+from nostr.subscription import Subscription
+
+def test_subscription_id():
+    subscription = Subscription(id=123, filters=Filters([Filter()]))
+    request = [ClientMessageType.REQUEST, subscription.id]
+    request.extend(subscription.filters.to_json_array())
+    message = json.dumps(request)
+    request_received = json.loads(message)
+    message_type, subscription_id, req_filters = request_received
+    assert isinstance(subscription_id, str)
+    assert message_type == ClientMessageType.REQUEST
+    assert isinstance(req_filters, dict)
