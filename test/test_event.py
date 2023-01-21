fd7c421817f1a11ffe3bc8fcd1911f159612ea6d
Ryan Armstrong <ryscar13@gmail.com>
2023-01-21 07:52:48 -0800

    cleaning up created_at default test

--- test/test_event.py
+++ test/test_event.py
@@ -3,7 +3,11 @@ from nostr.key import PrivateKey
 import time
 
 def test_event_default_time():
-    time.sleep(1.5)
+    """ensure created_at default value reflects the time
+    at Event object instantiation 
+    """
     public_key = PrivateKey().public_key.hex()
-    event = Event(public_key=public_key, content='test event')
-    assert (event.created_at - time.time()) < 1
+    event1 = Event(public_key=public_key, content='test event')
+    time.sleep(1.5)
+    event2 = Event(public_key=public_key, content='test event')
+    assert event1.created_at != event2.created_at
