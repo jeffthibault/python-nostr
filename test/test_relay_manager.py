de26816dfc88f995a0004acd7d7804cc4eb93a8f
Ryan Armstrong <ryscar13@gmail.com>
2023-01-21 08:58:14 -0800

    test that relay subscriptions are not linked

--- test/test_relay_manager.py
+++ test/test_relay_manager.py
@@ -1,6 +1,7 @@
 import pytest
 from nostr.event import Event
 from nostr.key import PrivateKey
+from nostr.subscription import Subscription
 from nostr.relay_manager import RelayManager, RelayException
 
 
@@ -28,3 +29,22 @@ def test_only_relay_valid_events():
     # Properly signed Event can be relayed
     pk.sign_event(event)
     relay_manager.publish_event(event)
+
+
+def test_separate_subscriptions():
+    """
+        make sure that subscription dictionary default is not the same object
+        across all relays so that subscriptions can vary
+    """
+    # initiate relay manager with two relays
+    relay_manager = RelayManager()
+    relay_manager.add_relay(url='fake-relay1')
+    relay_manager.add_relay(url='fake-relay2')
+
+    # make test subscription and add to one relay
+    test_subscription = Subscription(id='test')
+    relay_manager.relays['fake-relay1'].subscriptions.update(
+        {test_subscription.id: test_subscription}
+    )
+    # make sure test subscription isn't in second relay subscriptions
+    assert test_subscription.id not in relay_manager.relays['fake-relay2'].subscriptions.keys()
