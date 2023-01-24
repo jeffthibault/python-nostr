6287207149e1e7d9420bd82fcaa091e87ea12242
kdmukai <kdmukai@gmail.com>
2023-01-23 21:03:56 -0600

    Update test_event.py

--- test/test_event.py
+++ test/test_event.py
@@ -11,11 +11,24 @@ class TestEvent:
             ensure created_at default value reflects the time at Event object instantiation
             see: https://github.com/jeffthibault/python-nostr/issues/23
         """
-        public_key = PrivateKey().public_key.hex()
-        event1 = Event(public_key=public_key, content='test event')
+        event1 = Event(content='test event')
         time.sleep(1.5)
-        event2 = Event(public_key=public_key, content='test event')
+        event2 = Event(content='test event')
         assert event1.created_at < event2.created_at
+    
+
+    def test_add_event_ref(self):
+        some_event_id = "some_event_id"
+        event = Event(content="Adding an 'e' tag")
+        event.add_event_ref(some_event_id)
+        assert ['e', some_event_id] in event.tags
+
+
+    def test_add_pubkey_ref(self):
+        some_pubkey = "some_pubkey"
+        event = Event(content="Adding a 'p' tag")
+        event.add_pubkey_ref(some_pubkey)
+        assert ['p', some_pubkey] in event.tags
 
 
 
