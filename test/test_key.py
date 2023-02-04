69ff17b163f8af40f9514bcf7937964cb22997a2
kdmukai <kdmukai@gmail.com>
2023-02-04 08:50:48 -0600

    add EncryptedDirectMessage class; simplify Event class (#39)

--- test/test_key.py
+++ test/test_key.py
@@ -1,3 +1,4 @@
+from nostr.event import Event, EncryptedDirectMessage
 from nostr.key import PrivateKey
 
 
@@ -21,3 +22,78 @@ def test_from_nsec():
     pk1 = PrivateKey()
     pk2 = PrivateKey.from_nsec(pk1.bech32())
     assert pk1.raw_secret == pk2.raw_secret
+
+
+
+class TestEvent:
+    def setup_class(self):
+        self.sender_pk = PrivateKey()
+        self.sender_pubkey = self.sender_pk.public_key.hex()
+    
+
+    def test_sign_event_is_valid(self):
+        """ sign should create a signature that can be verified against Event.id """
+        event = Event(content="Hello, world!")
+        self.sender_pk.sign_event(event)
+        assert event.verify()
+
+
+    def test_sign_event_adds_pubkey(self):
+        """ sign should add the sender's pubkey if not already specified """
+        event = Event(content="Hello, world!")
+
+        # The event's public_key hasn't been specified yet
+        assert event.public_key is None
+
+        self.sender_pk.sign_event(event)
+
+        # PrivateKey.sign() should have populated public_key
+        assert event.public_key == self.sender_pubkey
+
+
+
+class TestEncryptedDirectMessage:
+    def setup_class(self):
+        self.sender_pk = PrivateKey()
+        self.sender_pubkey = self.sender_pk.public_key.hex()
+        self.recipient_pk = PrivateKey()
+        self.recipient_pubkey = self.recipient_pk.public_key.hex()
+
+
+    def test_encrypt_dm(self):
+        """ Should encrypt a DM and populate its `content` field with ciphertext that either party can decrypt """
+        message = "My secret message!"
+
+        dm = EncryptedDirectMessage(
+            recipient_pubkey=self.recipient_pubkey,
+            cleartext_content=message,
+        )
+
+        # DM's content field should be initially blank
+        assert dm.content is None
+        self.sender_pk.encrypt_dm(dm)
+
+        # After encrypting, the content field should now be populated
+        assert dm.content is not None
+
+        # Sender should be able to decrypt
+        decrypted_message = self.sender_pk.decrypt_message(encoded_message=dm.content, public_key_hex=self.recipient_pubkey)
+        assert decrypted_message == message
+
+        # Recipient should be able to decrypt by referencing the sender's pubkey
+        decrypted_message = self.recipient_pk.decrypt_message(encoded_message=dm.content, public_key_hex=self.sender_pubkey)
+        assert decrypted_message == message
+
+
+    def test_sign_encrypts_dm(self):
+        """ `sign` should encrypt a DM that hasn't been encrypted yet """
+        dm = EncryptedDirectMessage(
+            recipient_pubkey=self.recipient_pubkey,
+            cleartext_content="Some DM message",
+        )
+
+        assert dm.content is None
+
+        self.sender_pk.sign_event(dm)
+
+        assert dm.content is not None
